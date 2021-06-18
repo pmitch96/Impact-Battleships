2e51be7143058bd0c60a6c5155936ab258ee34f6
pmitch96 <85648033+pmitch96@users.noreply.github.com>
2021-06-18 15:28:16 +0100

    Deleted the classes I originally thought I needed but am no longer using.

--- src/BattleshipsPackage/Battleships.java
+++ src/BattleshipsPackage/Battleships.java
@@ -14,8 +14,6 @@ Ships Ethan = new Ships(3, "HMS Ethan");
 Ships Leon = new Ships(3, "HMS Leon");
 Ships Patrick = new Ships(2, "HMS Patrick");
 
-int NumberOfRows = 10;
-int NumberOfColumns = 10;
 int NumberOfShips = 8;
 
 String Ships = "Ship";
@@ -26,6 +24,7 @@ String Miss = "Miss";
 String[][] Board = new String[10][10];
 
 Arrays.fill(Board, Water);
+
 
 
 }
