a48a867efdcbb192f601f22fd34b3f20a235287b
pmitch96 <85648033+pmitch96@users.noreply.github.com>
2021-06-19 19:09:22 +0100

    Added a message that prints if you try to enter co-ordinates that were used in a previous round

--- src/BattleshipsPackage/Battleships.java
+++ src/BattleshipsPackage/Battleships.java
@@ -4,76 +4,84 @@ import java.util.Scanner;
 
 public class Battleships {
 
-public static String[][] Board = new String[10][10];
+    public static String[][] Board = new String[10][10];
 
-public static Scanner input = new Scanner(System.in);
+    public static Scanner input = new Scanner(System.in);
 
-public static void printBoard() {
-    for (int j = 0; j<= 9; j++){
-        for (int i = 0; i <= 9; i++) {
-            System.out.print(Board[j][i] + " ");
+    public static void printBoard() {
+        for (int j = 0; j <= 9; j++) {
+            for (int i = 0; i <= 9; i++) {
+                System.out.print(Board[j][i] + " ");
+            }
+            System.out.println();
         }
-        System.out.println();
     }
-}
 
- public static void main(String[] args) {
+    public static void main(String[] args) {
 //  Ships HMS_Gareth = new Ships(4);
 //  Ships HMS_Jordan = new Ships(3);
 
-  String water = "~";
-  String hit = "X";
-  String miss = "O";
-  String ship = "S";
+        String water = "~";
+        String hit = "X";
+        String miss = "O";
+        String ship = "*";
 
 
-for (String[] row : Board){
-    Arrays.fill(row, water);
-}
+        for (String[] row : Board) {
+            Arrays.fill(row, water);
+        }
 
 
 //Placing HMS_Gareth
 
-  for (int i = 0; i <= 3; i++) {
-   Board[0][i] = ship;
-  }
+        for (int i = 0; i <= 3; i++) {
+            Board[0][i] = ship;
+        }
 
 //Placing HMS_Jordan
 
-  for (int i = 0; i <= 2; i++) {
-   Board[3][i] = ship;
-  }
+        for (int i = 0; i <= 2; i++) {
+            Board[3][i] = ship;
+        }
 
 
-printBoard();
+        printBoard();
 
+        int NumberHits = 0;
 
-  int NumberHits = 0;
+        while (NumberHits < 7) {
 
-  System.out.println("Please input the x co-ordinate of your guess");
-  int x_guess = input.nextInt();
-  System.out.println("Please input the y co-ordinate of your guess");
-  int y_guess = input.nextInt();
-  String OutPut = Board[x_guess][y_guess];
+            System.out.println("Please input the x co-ordinate of your guess");
+            int x_guess = input.nextInt();
+            System.out.println("Please input the y co-ordinate of your guess");
+            int y_guess = input.nextInt();
+            String OutPut = Board[x_guess][y_guess];
 
-  if (OutPut == ship) {
-   System.out.println("Hit!");
-   Board[x_guess][y_guess] = hit;
-   NumberHits = NumberHits + 1;
+            if (OutPut == ship) {
+                System.out.println("Hit!");
+                Board[x_guess][y_guess] = hit;
+                NumberHits = NumberHits + 1;
+                printBoard();
+            }
+            else if (OutPut == miss){
+                System.out.println("Please enter co-ordinates that haven't been used before");
+            }
+            else if (OutPut == hit){
+                System.out.println("Please enter co-ordinates that haven't been used before");
+            }
+            else {
+                System.out.println("Miss");
+                Board[x_guess][y_guess] = miss;
+                printBoard();
+            }
 
-  } else {
-   System.out.println("Miss");
-   Board[x_guess][y_guess] = miss;
-  }
+            if (NumberHits == 7) {
+                System.out.println("Game Over!");
+            }
 
-  if (NumberHits == 1 ) {
-      System.out.println("Game Over!");
-  }
+        }
 
-     printBoard();
-
- }
-
+    }
 }
 
 
